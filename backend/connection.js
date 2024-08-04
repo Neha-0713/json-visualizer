d9b376f1fca2989d00bc667f44a420aefbae956c
Neha-0713 <nrai52703@gmail.com>
2024-08-04 16:47:14 +0530

    setup

---
+++ backend/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://neha:Neha200402@cluster0.70lw4rp.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0"
+
+
+//asynchronous function- they return promise object and then catch is used to to check if there is any error
+mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');
+})
+.catch((err) => {
+    console.log(err);
+});
+
+module.exports = mongoose;
